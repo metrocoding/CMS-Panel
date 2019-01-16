74f724508b65b5a911ec49c187697ba3d9209c86
Code2heaven <dead4game@gmail.com>
2019-01-16 12:57:21 +0330

    new

--- admin/slide/alter_slide.aspx.cs
+++ admin/slide/alter_slide.aspx.cs
@@ -54,8 +54,10 @@ public partial class admin_slide_alter_slide : System.Web.UI.Page
         //fill form
         slide_img.Src = "~/images/uploads/slides/full/" + dt.Rows[0][7];
         sli_title.Value = dt.Rows[0][1].ToString();
-        sli_start.Value = dt.Rows[0][2].ToString();
-        sli_end.Value = dt.Rows[0][3].ToString();
+        sli_start.Value = dt.Rows[0][2].ToString().Insert(4, "/").Insert(7, "/");
+        sli_end.Value = dt.Rows[0][3].ToString().Insert(4, "/").Insert(7, "/");
+        ListItem li = sli_page.Items.FindByValue(dt.Rows[0][5].ToString());
+        li.Selected = true;
         sli_order.Value = dt.Rows[0][4].ToString();
         sli_des.Value = dt.Rows[0][8].ToString();
     }
@@ -103,8 +105,8 @@ public partial class admin_slide_alter_slide : System.Web.UI.Page
         SqlCommand command = new SqlCommand("update slide set title=@p1,startDate=@p2,endDate=@p3,slideOrder=@p4,targetPage=@p5,show=@p6,pic = case when @p7='' then pic else @p7 end ,describe=@p8 where id=@id", connection);
 
         command.Parameters.AddWithValue("@p1", sli_title.Value);
-        command.Parameters.AddWithValue("@p2", sli_start.Value);
-        command.Parameters.AddWithValue("@p3", sli_end.Value);
+        command.Parameters.AddWithValue("@p2", sli_start.Value.Replace("/", ""));
+        command.Parameters.AddWithValue("@p3", sli_end.Value.Replace("/", ""));
         command.Parameters.AddWithValue("@p4", sli_order.Value);
         command.Parameters.AddWithValue("@p5", sli_page.Value);
         command.Parameters.AddWithValue("@p6", show.Checked);
